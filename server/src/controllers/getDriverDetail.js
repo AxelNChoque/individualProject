0470471d2b5896c07d48148508e86ccb577617f4
AxelChoque <axel_chooque@hotmail.com>
2023-09-23 14:11:02 -0300

    rutes 2/5

--- server/src/controllers/getDriverDetail.js
+++ server/src/controllers/getDriverDetail.js
@@ -1,12 +1,23 @@
 const axios = require('axios');
+const { Driver , Team } = require('../db');
 
 const getDriverDetail = async (req, res) => {
     try {
         const { id } = req.params;
-        const response = await axios.get(`http://localhost:5000/drivers/:${id}`);
-        const driver = response.data;
-        if(!driver) return res.status(400).json({error: 'conductor no encontrado'});
-        
+
+        const driverDB = await Driver.findByPk(id, {
+            include: Team,
+        });
+
+        if(driverDB) return res.status(200).json({driverDB});
+
+
+        const response = await axios.get(`http://localhost:5000/drivers/${id}`);
+        const driverAPI = response.data;
+        if(driverAPI) {
+            res.status(200).json({driverAPI});
+        }
+        return res.status(400).json({error: 'conductor no encontrado'});
     } catch(error) {
         res.status(400).json({error:error.message});
     }
